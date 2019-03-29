3d322a8866640b0583ec2b0fad0b118fd3b7b9e0
Luis Trinta <33879012+luistrinta@users.noreply.github.com>
2019-03-29 01:21:29 +0000

    Ta melhor , mas não ta a pintar :(

--- Node.java
+++ Node.java
@@ -3,11 +3,10 @@ import java.lang.*;
 import java.util.Random;
 public class Node extends Tabela {
 
-    int action;
     int visits; // number of times visited  N(v)
     int reward; // accumulated reward value Q(v)
     Node parent; // null if root
-    LinkedList<Node> filhos;
+    Node[] filhos;
     boolean[] expanded ;
     Tabela tabela;
 
@@ -18,7 +17,7 @@ public class Node extends Tabela {
         visits = 1;
         reward = 0;
         parent = null;
-        filhos = new LinkedList<Node>();
+        filhos = new Node[7];
         tabela = new Tabela();
     }
 
@@ -28,7 +27,7 @@ public class Node extends Tabela {
         visits = 1;
         reward = 0;
         parent = null;
-        filhos = new LinkedList<Node>();
+        filhos = new Node[7];
         tabela = new Tabela(t);
     }
 
@@ -39,115 +38,180 @@ public class Node extends Tabela {
         visits = n.visits;
         reward = n.reward;
         parent = n;
-        n.filhos.add(this);
-        filhos = new LinkedList<Node>();
+        filhos = new Node[7];
         tabela = new Tabela(t);
     }
-
-
-    // update node and backpropagate to parent
-
-    void addChild(Node child) {
-        this.filhos.add(child);   // add child node
-    }
-
-
-
-    //legal actions
-    public  ArrayList<Integer> movimentosPossiveis() {
-
-        ArrayList<Integer> filhos = new ArrayList<Integer>();
-
-        for(int i = 0 ; i < 7 ; i++ ) {
-            if(this.arr[0][i] == '-')
-                filhos.add(i);
-        }
-        return filhos;
-    }
-
-
-    //applyAction: A
-    //play(tabela , 'x' , i );
-
-    //playout:  f
-    //if(isWinner(t,'x')) node reward plus 1
-    //if(isWinner(t,'o')) node reward minus 1
-    //  else draw
-
 
 
 
 
     public static int MCTS_Search(Tabela tabela) {
-        long n = 0;
         Node root = new Node(tabela);
 
+        long n = 0;
+
+        for (long stop = System.currentTimeMillis()+5000; stop>System.currentTimeMillis();) {
+        Node selectedNode = select(root);
+
+        Node expandedNode = expandNode(selectedNode);
+        
+                    double resultado = simulate(expandedNode.tabela);
+                    //System.out.println(resultado);
+
+                    backPropagate(expandedNode, resultado);
+
+                }
+           
+        int indexMax = -1;
+        for(int i = 0; i < 7; i++) {
+            if(root.filhos[i] != null) {
+                if(indexMax == -1 || root.filhos[i].visits > root.filhos[indexMax].visits)
+                    indexMax = i;
+                // System.out.printf("\nlocation%d: p1wins: %f/%d = %f", i, root.children[i].player1Wins, root.children[i].visits, root.children[i].player1Wins/root.children[i].visits);
+            }
+        }
+        // System.out.println();*/
+        return indexMax;
+    }
 
 
-        while(n < 5000) {
-
-            Node next = Select(root);
-
-            int valorização = Simmulate(next.tabela); //valorização do tabela do filho
-
-            next = backUp(next, valorização);
 
 
-            n++;
+
+
+    public static Node select(Node selectedNode) {
+
+        for(int i = 0; i < 7; i++) {
+            if(selectedNode.filhos[i] == null && canPlace(selectedNode.tabela, i)) {
+                //System.out.println("Penis");
+                return selectedNode;
+            }
+        }
+        //Se os filhos ja tiverem dados , vemos qual o proximo a abordar
+        double valorMax = -1;
+        int indexMax = -1;
+        for(int i = 0; i < 7; i++) {
+            //System.out.println("Entrou");
+            //se não conseguirmos introduzir um valor numa determinada coluna , continuamos
+            if(!canPlace(selectedNode.tabela, i))
+                continue;
+
+            Node currentChild = selectedNode.filhos[i];
+            double wins;
+            if(selectedNode.tabela.lastplayer == 'o')
+                wins = currentChild.reward;
+            else
+                wins = currentChild.visits - currentChild.reward;
+
+            double valorSelecao = wins / currentChild.visits + Math.sqrt(2) * Math.sqrt(Math.log(selectedNode.visits) / currentChild.visits); // UCT
+
+            if(valorSelecao > valorMax) {
+                valorMax = valorSelecao;
+                indexMax = i;
+            }
+        }
+        // SOMETIMES -1???
+        if(indexMax == -1)
+            return null;
+
+        return select(selectedNode.filhos[indexMax]);
+    }
+
+
+
+    public static Node expandNode(Node selectedNode) {
+
+        //Recebe uma lista com os filhos não visitados
+        ArrayList<Integer> unvisitedChildren = new ArrayList<Integer>(7);
+
+        for(int i = 0 ; i < 7 ; i++) {
+            if(selectedNode.filhos[i] == null && getExpandMoves(selectedNode.tabela).contains(i)) {
+                //System.out.println("O filho na posição " + i + " não existe");
+                unvisitedChildren.add(i);
+            }
+        }
+
+        //Escolher um filho aleatório e criar um nó para ele
+        int selectedIndex = unvisitedChildren.get((int)(Math.random() * unvisitedChildren.size()));
+        //System.out.println(selectedIndex);//teste
+
+
+        if(selectedNode.tabela.lastplayer == 'N') {
+            selectedNode.filhos[selectedIndex] = new Node(play(selectedNode.tabela, 'x', selectedIndex), selectedNode);
+        } 
+        else if(selectedNode.tabela.lastplayer == 'x')
+            selectedNode.filhos[selectedIndex] = new Node(play(selectedNode.tabela, 'o', selectedIndex), selectedNode);
+
+        else if(selectedNode.tabela.lastplayer == 'o')
+            selectedNode.filhos[selectedIndex] =  new Node(play(selectedNode.tabela, 'x', selectedIndex), selectedNode);
+
+
+        //retorna o filho criado
+        return selectedNode.filhos[selectedIndex];
+    }
+
+
+
+
+    public static double simulate(Tabela t) { //simulate
+
+        Tabela t0 = new Tabela(t);
+
+        while(!isCompleted(t0) && !checkWinners(t0, 'x') && !checkWinners(t0, 'o') ) {
+            Random rand = new Random();
+            int randomNum = rand.nextInt(getExpandMoves(t0).size());
+            int aleatoria = (getExpandMoves(t0)).get(randomNum);//escolhe um numero aleatório das jogadas possiveis
+            if(t0.lastplayer == 'o') {              //Caso a ultima jogada tenha sido o , ele vai jogar a contrária
+                t0 = play(t0, 'x', aleatoria);
+                // printTabela(t0);
+            }
+
+            else if(t0.lastplayer == 'x') { //Caso a ultima jogada tenha sido x , ele vai jogar a contrária
+                t0 = play(t0, 'o', aleatoria);
+                //printTabela(t0);
+            }
 
         }
 
-      
-
-
-        System.out.println( bestChild(root).reward);
-
-        return bestChild(root).tabela.bestY;
-    }
-
-
-    public static Node Select(Node node) {
-        while(isCompleted(node.tabela) == false && checkWinners(node.tabela, 'x') == false && checkWinners(node.tabela, 'o') == false) {
-
-            if(fullyExpanded(node) == false) {
-               // System.out.println("Not fully Expanded");
-                return  Expand(node);
-
-            }
-
-            else {
-                node = bestChild(node);
-            }
+        if(checkWinners(t0, 'x')) {
+            //System.out.println("x wins");
+            return 1;
         }
-        return node;
+
+        if(checkWinners(t0, 'o')) {
+            //System.out.println("o wins");
+            return 0;
+        }
+
+        else {
+           // System.out.println("Tie");
+            return 0;
+        }
     }
 
-    //Auxiliar da Select
-    public static double calculateScore(Node child, Node father) {
 
-        double val = Math.sqrt(2);
+    public static void backPropagate(Node expandedNode, double resultado) {
+        Node node = expandedNode;
 
+        while(node.parent != null) {
+            node.visits++;
 
-        return ((child.reward / child.visits) + val * Math.sqrt((2 * Math.log(father.visits)) / child.visits));
-    }
+            node.reward += resultado;
 
-    public static Node bestChild(Node father) {
-        Node best = new Node();
-            best.reward = Integer.MIN_VALUE;
-        for(Node n0 : father.filhos) {
-            //System.out.println(n0.reward + " " + n0.tabela.lastMoveY);
-            //if(calculateScore(n0, father) > calculateScore(best, father)) {
-              if(best.reward < n0.reward){
-                best = n0;
-                best.tabela.bestY = n0.tabela.lastMoveY;
-            }
-            
+            node = node.parent;
+        }
 
     }
-        //printTabela(best.tabela);
-        //System.out.println(best.tabela.bestY);
-        return best; //fórmula matemática
-}
+
+
+
+
+
+    public static boolean canPlace(Tabela t, int i) {
+        if(t.arr[0][i] == '-')return true;
+
+        else return false;
+    }
 
     //verifica se tem os fihos todos  //NAO TA OPERACIONAL
     public static boolean fullyExpanded(Node n) {
@@ -163,13 +227,22 @@ public class Node extends Tabela {
 
         return true;
     }
+    //Auxiliar do Expand
+
+    public static ArrayList<Integer> getExpandMoves(Tabela t) {
+        ArrayList<Integer> actions = new ArrayList<Integer>();
+        for(int j = 0; j < 7; j++)
+            if(t.arr[0][j] == '-')
+                actions.add(j);
+        return actions;
+    }
 
     public static Node Expand(Node n) {
         Random rand = new Random();
         //Expande o 'x' e de seguida simula uma jogada 'x' e uma 'o'
 
         int aleatoria = getExpandMoves(n.tabela).get(rand.nextInt(n.tabela.getMoves().size()));//escolhe um numero aleatório das jogadas possiveis
-        
+
         //System.out.println("Jogada aleatória : " + aleatoria);
 
         Node novoFilho = new Node(n.tabela, n) ;
@@ -182,8 +255,8 @@ public class Node extends Tabela {
 
 
         else if(n.tabela.lastplayer == 'o') { //Caso a ultima jogada tenha sido o , ele vai jogar a contrária
-                (novoFilho.tabela) = play(n.tabela, 'x', aleatoria);
-                n.expanded[aleatoria] = true;
+            (novoFilho.tabela) = play(n.tabela, 'x', aleatoria);
+            n.expanded[aleatoria] = true;
         }
 
         else if(n.tabela.lastplayer == 'x') { //Caso a ultima jogada tenha sido x , ele vai jogar a contrária
@@ -192,77 +265,16 @@ public class Node extends Tabela {
         }
 
         //printTabela(novoFilho.tabela);
-       
+
         return novoFilho;
     }
 
-    //Auxiliar do Expand
 
-    public static ArrayList<Integer> getExpandMoves(Tabela t) {
-        ArrayList<Integer> actions = new ArrayList<Integer>();
-        for(int j = 0; j < 7; j++)
-            if(t.arr[0][j] == '-')
-                actions.add(j);
-        return actions;
-    }
-
-
-    public static Node backUp(Node n, int val) {
-        //System.out.println("Backpropagation");
-        while(n.parent != null) {
-            n.visits++;
-          //  System.out.println("Number of visits : "+n.visits);
-            n.reward += val;
-          //  System.out.println("Reward points : "+ n.reward);
-          //  System.out.println("Table ");
-          //  printTabela(n.tabela);
-          //  System.out.println("Score : "+calculateScore(n,n.parent));
-            n = n.parent;
-        }
-        return n;
-    }
-
-    public static int Simmulate(Tabela t) { //Simmulate
-       
-        Tabela t0 = new Tabela(t);
-
-        while(!isCompleted(t0) && !checkWinners(t0, 'x') && !checkWinners(t0, 'o') ) {
-            Random rand = new Random();
-            int randomNum = rand.nextInt(getExpandMoves(t0).size());
-            int aleatoria = (getExpandMoves(t0)).get(randomNum);//escolhe um numero aleatório das jogadas possiveis
-            if(t0.lastplayer == 'o') {              //Caso a ultima jogada tenha sido o , ele vai jogar a contrária
-                t0 = play(t0, 'x', aleatoria);
-               // printTabela(t0);
-            }
-
-            else if(t0.lastplayer == 'x') { //Caso a ultima jogada tenha sido x , ele vai jogar a contrária
-                t0 = play(t0, 'o', aleatoria);
-                //printTabela(t0);
-            }
-
-        }
-      
-        if(checkWinners(t0, 'x')) {
-            System.out.println("x wins");
-            return 1;
-        }
-
-        if(checkWinners(t0, 'o')) {
-            System.out.println("o wins");
-            return -1;
-        }
-
-        else {
-            System.out.println("Tie");
-            return 0;
-        }
-    }
 
 
 
     public static void main(String[] args) {
         Tabela t = new Tabela();
-        
         while(!isCompleted(t)) {
             
 
@@ -281,7 +293,7 @@ public class Node extends Tabela {
                 System.out.println("Player o won");
                 return;
             }
-            //int x = MCTS_Search(t);
+            
             System.out.println("Computador jogou na :"+t.bestY);
             t = play(t, 'x', MCTS_Search(t));
             printTabela(t);
@@ -302,7 +314,6 @@ public class Node extends Tabela {
         }
 
         System.out.println("Não há vencedores");
-
 
     }
 }
